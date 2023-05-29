140fd3001456fd357a73de6e6cf913995dc064ea
Albert <albertvitt1997@gmail.com>
2023-05-29 17:54:30 +0700

    Follow_model

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -5,44 +5,53 @@ from django.db.models import Q, F
 User = get_user_model()
 
 
-class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField('Дата публикации',
-                                    auto_now_add=True)
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE,
-        related_name='posts',
-        verbose_name='Автор поста')
-    image = models.ImageField(
-        upload_to='posts/',
-        null=True,
-        blank=True)
-
-    def __str__(self):
-        return self.text
-
-
 class Group(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True,
+                            null=False)
     description = models.TextField()
 
     def __str__(self):
         return self.title
 
 
-class Comment(models.Model):
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE,
-        related_name='comments',
-        verbose_name='Автор комментария')
-    post = models.ForeignKey(
-        Post, on_delete=models.CASCADE,
-        related_name='comments',
-        verbose_name='Коментарий')
+class Post(models.Model):
     text = models.TextField()
-    created = models.DateTimeField(
-        'Дата добавления', auto_now_add=True, db_index=True)
+    pub_date = models.DateTimeField('Дата публикации',
+                                    auto_now_add=True)
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='posts',
+                               verbose_name='Автор поста')
+    image = models.ImageField(upload_to='posts/',
+                              null=True,
+                              blank=True)
+    group = models.ForeignKey(Group,
+                              on_delete=models.SET_NULL,
+                              related_name='posts',
+                              verbose_name='Группа',
+                              null=True,
+                              blank=True)
+
+    def __str__(self):
+        return self.text
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='comments',
+                               verbose_name='Автор комментария')
+    post = models.ForeignKey(Post,
+                             on_delete=models.CASCADE,
+                             blank=True,
+                             null=True,
+                             related_name='comments',
+                             verbose_name='Коментарий')
+    text = models.TextField()
+    created = models.DateTimeField('Дата добавления',
+                                   auto_now_add=True,
+                                   db_index=True)
 
 
 class Follow(models.Model):
@@ -51,19 +60,19 @@ class Follow(models.Model):
                              related_name='follower',
                              verbose_name='подпсикчик'
                              )
-    author = models.ForeignKey(User,
-                               on_delete=models.CASCADE,
-                               related_name='following',
-                               verbose_name='Блогер'
-                               )
+    following = models.ForeignKey(User,
+                                  on_delete=models.CASCADE,
+                                  related_name='following',
+                                  verbose_name='Блогер'
+                                  )
 
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=['user', 'author'],
+            models.UniqueConstraint(fields=['user', 'following'],
                                     name='unique_follow'),
-            models.CheckConstraint(check=~Q(user=F('author')),
+            models.CheckConstraint(check=~Q(user=F('following')),
                                    name='no_self_follow')
         ]
 
     def __str__(self):
-        return (f'{self.user.username} -> {self.author.username}')
+        return (f'{self.user.username} -> {self.following.username}')
