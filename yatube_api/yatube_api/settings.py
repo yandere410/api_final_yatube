ca388c7bc8e7525df97e4e7f63166113bfb98f7c
Albert <albertvitt1997@gmail.com>
2023-05-30 21:56:52 +0700

    change_default_perm

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -92,7 +92,7 @@ STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
