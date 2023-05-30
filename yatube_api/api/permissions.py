8c6f7ef2d3707a01be7c608ce3dd11748dc294c9
Albert <albertvitt1997@gmail.com>
2023-05-30 21:58:07 +0700

    add_has_permissions

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -3,6 +3,11 @@ from rest_framework import permissions
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
     """Object-level permission to only allow owners of an object to edit."""
+
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
                 or obj.author == request.user)
+
+    def has_permission(self, request, view):
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated)
